0905cd1629033bb57cacedba867182e73382ae49
baduhai <baduhai@pm.me>
2022-12-20 14:08:50 -0300

    changed secret to fix acme

--- hosts/servers/alexandria/security.nix
+++ hosts/servers/alexandria/security.nix
@@ -9,10 +9,10 @@
       email = "baduhai@proton.me";
       dnsResolver = "1.1.1.1:53";
       dnsProvider = "cloudflare";
+      credentialsFile = config.age.secrets.cloudflare-creds.path;
     };
     certs."baduhai.me" = {
       extraDomainNames = [ "*.baduhai.me" ];
-      credentialsFile = config.age.secrets.cloudflare-creds.path;
     };
   };
 }
