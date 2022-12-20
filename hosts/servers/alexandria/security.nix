4e4c9c6c3571f20f714cf1d6a29ee41ef755fcc8
baduhai <baduhai@pm.me>
2022-12-20 14:02:14 -0300

    Mores acme fixes

--- hosts/servers/alexandria/security.nix
+++ hosts/servers/alexandria/security.nix
@@ -9,10 +9,10 @@
       email = "baduhai@proton.me";
       dnsResolver = "100.100.100.100:53";
       dnsProvider = "cloudflare";
-      credentialsFile = config.age.secrets.cloudflare-creds.path;
     };
     certs."baduhai.me" = {
       extraDomainNames = [ "*.baduhai.me" ];
+      credentialsFile = config.age.secrets.cloudflare-creds.path;
     };
   };
 }
