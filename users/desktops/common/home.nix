853472ea3531912271dee3826ea3eee7a0dcb242
baduhai <baduhai@pm.me>
2022-12-22 23:39:27 -0300

    Removed flatpak references

--- users/desktops/common/home.nix
+++ users/desktops/common/home.nix
@@ -21,9 +21,6 @@
       # Autostart programs
       ".config/autostart/org.kde.yakuake.desktop".source = config.lib.file.mkOutOfStoreSymlink "/var/run/current-system/sw/share/applications/org.kde.yakuake.desktop";
       ".config/autostart/koi.desktop".source = config.lib.file.mkOutOfStoreSymlink "/var/run/current-system/sw/share/applications/koi.desktop";
-      # Fix flatpak fonts, themes, icons and cursor
-      ".icons/breeze_cursors".source = config.lib.file.mkOutOfStoreSymlink "/run/current-system/sw/share/icons/breeze_cursors";
-      ".local/share/flatpak/overrides/global".text = "[Context]\nfilesystems=/run/current-system/sw/share/X11/fonts:ro;~/.local/share/color-schemes:ro;xdg-config/gtk-3.0:ro;/nix/store:ro;~/.icons:ro";
     };
   };
 }
