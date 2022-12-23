6bd0acbd0162247760a8b65e983cbe90aad26853
baduhai <baduhai@pm.me>
2022-12-23 00:16:06 -0300

    remove kitty evidence

--- users/desktops/common/home.nix
+++ users/desktops/common/home.nix
@@ -16,8 +16,6 @@
       # Dotfiles that can't be managed via home-manager
       ".local/share/color-schemes/BreezeDarkNeutral.colors".source = "${inputs.dotfiles}/color-schemes/BreezeDarkNeutral.colors";
       ".config/MangoHud/MangoHud.conf".source = "${inputs.dotfiles}/MangoHud/MangoHud.conf";
-      ".config/kitty/search.py".source = "${inputs.dotfiles}/kitty/search.py";
-      ".config/kitty/scroll_mark.py".source = "${inputs.dotfiles}/kitty/scroll_mark.py";
       # Autostart programs
       ".config/autostart/org.kde.yakuake.desktop".source = config.lib.file.mkOutOfStoreSymlink "/var/run/current-system/sw/share/applications/org.kde.yakuake.desktop";
       ".config/autostart/koi.desktop".source = config.lib.file.mkOutOfStoreSymlink "/var/run/current-system/sw/share/applications/koi.desktop";
