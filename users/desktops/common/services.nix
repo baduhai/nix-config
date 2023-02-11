05dac860fa0b10a172013d97b2bfd51c6777bdd9
rotterdam <foxtrot@rotterdam>
2023-02-11 09:22:22 -0300

    removed syncthing tray

--- users/desktops/common/services.nix
+++ users/desktops/common/services.nix
@@ -3,12 +3,5 @@
 {
   services = {
     kdeconnect.enable = true;
-    syncthing = {
-      enable = true;
-      tray = {
-        enable = true;
-        package = pkgs.writeShellScriptBin "syncthingtray" "exec ${pkgs.syncthingtray}/bin/syncthingtray --wait" // { pname = "syncthingtray"; }; # Override synctray so it waits for a system tray
-      };
-    };
   };
 }
