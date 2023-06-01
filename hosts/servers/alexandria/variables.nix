5f521771b74c439443ef30298e567d7525cbdedb
rotterdam <foxtrot@rotterdam>
2023-06-01 20:09:36 -0300

    fix big-agi port

--- hosts/servers/alexandria/variables.nix
+++ hosts/servers/alexandria/variables.nix
@@ -22,7 +22,7 @@ in {
     searx = mkStringOption "8007";
     qbittorrent = mkStringOption "8008";
     actual = mkStringOption "8009";
-    chatbot-ui = mkStringOption "8010";
+    big-agi = mkStringOption "8010";
     jellyfin = mkStringOption "8096";
     sonarr = mkStringOption "8989";
     jackett = mkStringOption "9117";
