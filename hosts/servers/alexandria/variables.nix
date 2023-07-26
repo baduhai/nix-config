e62e5279f883b9c7da6724c56e81ba789336b893
William <baduhai@proton.me>
2023-07-26 16:48:29 -0300

    Drop shiori on Alexandria

--- hosts/servers/alexandria/variables.nix
+++ hosts/servers/alexandria/variables.nix
@@ -9,7 +9,6 @@ let
 
 in {
   options.ports = {
-    n8n = mkStringOption "5678";
     bazaar = mkStringOption "6767";
     radarr = mkStringOption "7878";
     vaultwarden = mkStringOption "8000";
@@ -17,13 +16,12 @@ in {
     cinny = mkStringOption "8002";
     librespeed = mkStringOption "8003";
     paperless = mkStringOption "8004";
-    shiori = mkStringOption "8005";
+    podsync = mkStringOption "8005";
     cinny2 = mkStringOption "8006";
     searx = mkStringOption "8007";
     qbittorrent = mkStringOption "8008";
     actual = mkStringOption "8009";
     pairdrop = mkStringOption "8010";
-    podsync = mkStringOption "8011";
     jellyfin = mkStringOption "8096";
     sonarr = mkStringOption "8989";
     jackett = mkStringOption "9117";
