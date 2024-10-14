671dbfdb95b6bf7fdd8eafedec3deb8aca4f60f1
William <baduhai@proton.me>
2024-10-14 09:03:05 -0300

    nextcloud 30

--- hosts/servers/alexandria/nextcloud.nix
+++ hosts/servers/alexandria/nextcloud.nix
@@ -4,7 +4,7 @@
   services = {
     nextcloud = {
       enable = true;
-      package = pkgs.nextcloud29;
+      package = pkgs.nextcloud30;
       datadir = "/data/nextcloud";
       hostName = "nextcloud.baduhai.dev";
       configureRedis = true;
