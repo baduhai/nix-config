fae754891e9f14432933f99dde50cad62d4c4c96
William <baduhai@proton.me>
2025-03-22 17:18:16 -0300

    changed the way modules are imported on hosts

--- hosts/alexandria.nix
+++ hosts/alexandria.nix
@@ -5,17 +5,7 @@
 
   imports = [
     ./modules/alexandria
-    ./modules/boot.nix
-    ./modules/console.nix
-    ./modules/desktop.nix
-    ./modules/locale.nix
-    ./modules/networking.nix
-    ./modules/nix.nix
-    ./modules/programs.nix
-    ./modules/security.nix
-    ./modules/services.nix
-    ./modules/users.nix
-    ./modules/virtualisation.nix
+    ./modules
   ];
 
   nix.nixPath = [ "nixos-config=${./alexandria.nix}" ];
