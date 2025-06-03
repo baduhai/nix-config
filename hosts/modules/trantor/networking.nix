63a32cee922c31edee8a9c097fcd28533a7fe802
William <baduhai@proton.me>
2025-06-02 21:00:26 -0300

    drop nat to alexandria; open 25566

--- hosts/modules/trantor/networking.nix
+++ hosts/modules/trantor/networking.nix
@@ -3,34 +3,8 @@
 {
   networking = {
     firewall = {
-      allowedTCPPorts = [ 25565 ];
-      allowedUDPPorts = [
-        25565
-        19132
-      ];
-    };
-    nat = {
-      enable = true;
-      externalInterface = "enp0s6";
-      internalInterfaces = [ "tailscale0" ];
-      externalIP = null;
-      forwardPorts = [
-        {
-          sourcePort = 25565;
-          proto = "tcp";
-          destination = "100.76.19.50:25565";
-        }
-        {
-          sourcePort = 25565;
-          proto = "udp";
-          destination = "100.76.19.50:25565";
-        }
-        {
-          sourcePort = 19132;
-          proto = "udp";
-          destination = "100.76.19.50:19132";
-        }
-      ];
+      allowedTCPPorts = [ 25566 ];
+      allowedUDPPorts = [ 25566 ];
     };
   };
 }
