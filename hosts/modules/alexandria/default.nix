968f28581445928f1c94dc072376d2635cdd85c2
William <baduhai@proton.me>
2025-06-08 20:47:59 -0300

    remove references to deleted files

--- hosts/modules/alexandria/default.nix
+++ hosts/modules/alexandria/default.nix
@@ -2,13 +2,8 @@
 
 {
   imports = [
-    ./forgejo.nix
     ./hardware-configuration.nix
-    ./jellyfin.nix
-    ./librespeed.nix
-    ./nginx.nix
     ./services.nix
     ./users.nix
-    ./vaultwarden.nix
   ];
 }
--- hosts/modules/alexandria/users.nix
+++ hosts/modules/alexandria/users.nix
@@ -1,19 +1,5 @@
 { ... }:
 
 {
-  users = {
-    users = {
-      nginx.extraGroups = [ "acme" ];
-    };
-    groups = {
-      hosted = {
-        gid = 1005;
-        members = [
-          "user"
-          "paperless"
-          "vaultwarden"
-        ];
-      };
-    };
-  };
+  users.users.nginx.extraGroups = [ "acme" ];
 }
