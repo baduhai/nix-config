ab9314be6d9b46cef68caaafdf4f55dbca82d38f
William <baduhai@proton.me>
2025-07-02 16:14:42 -0300

    add radicale service

--- hosts/modules/alexandria/services.nix
+++ hosts/modules/alexandria/services.nix
@@ -2,9 +2,10 @@
 
 let
   ports = {
-    vaultwarden = "8000";
-    librespeed = "8001";
     jellyfin = "8096";
+    librespeed = "8000";
+    radicale = "8001";
+    vaultwarden = "8002";
   };
 in
 
@@ -26,6 +27,10 @@ in
         in
         lib.mapAttrs (_: lib.recursiveUpdate commonVHostConfig) {
           "_".locations."/".return = "444";
+          "dav.baduhai.dev".locations."/" = {
+            proxyPass = "http://127.0.0.1:${ports.radicale}";
+            extraConfig = "proxy_pass_header Authorization;";
+          };
           "git.baduhai.dev".locations."/".proxyPass =
             "http://unix:${config.services.forgejo.settings.server.HTTP_ADDR}:/";
           "jellyfin.baduhai.dev".locations."/".proxyPass = "http://127.0.0.1:${ports.jellyfin}";
@@ -55,6 +60,23 @@ in
     jellyfin = {
       enable = true;
       openFirewall = true;
+    };
+
+    radicale = {
+      enable = true;
+      settings = {
+        server = {
+          hosts = [
+            "127.0.0.1:5232"
+            "[::]:5232"
+          ];
+        };
+        auth = {
+          type = "htpasswd";
+          htpasswd_filename = "/etc/radicale/users";
+          htpasswd_encryption = "bcrypt";
+        };
+      };
     };
 
     vaultwarden = {
