98c46ded573d42a22a70d095847412917be4f84e
William <baduhai@proton.me>
2025-07-02 15:43:31 -0300

    moving to stable kernel

--- hosts/modules/boot.nix
+++ hosts/modules/boot.nix
@@ -38,7 +38,7 @@
         plymouth.enable = true;
         initrd.systemd.enable = true;
         loader.efi.efiSysMountPoint = "/boot/efi";
-        kernelPackages = pkgs.linuxPackages_xanmod_latest;
+        kernelPackages = pkgs.linuxPackages_xanmod_stable;
         extraModprobeConfig = ''
           options bluetooth disable_ertm=1
         '';
