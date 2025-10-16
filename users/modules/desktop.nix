02eb626d336c933d92cfabd24aaaa431c367444c
William <baduhai@proton.me>
2025-10-16 10:03:29 -0300

    ghostty settings in hm

--- users/modules/desktop.nix
+++ users/modules/desktop.nix
@@ -11,27 +11,22 @@
       enableVPN = false;
     };
 
-    rio = {
-      enable = true;
-      settings = {
-        theme = "catppuccin-mocha";
-        fonts = {
-          family = "FiraCode Nerd Font";
-          size = 16.0;
-          emoji.family = "Noto Color Emoji";
-        };
-        confirm-before-quit = false;
-        window = {
-          width = 1121;
-          height = 633;
-        };
-      };
-    };
-
     ghostty = {
       enable = true;
       enableFishIntegration = true;
       enableBashIntegration = true;
+      settings = {
+        cursor-style = "block";
+        shell-integration-features = "no-cursor";
+        cursor-style-blink = false;
+        custom-shader = "${builtins.fetchurl {
+          url = "https://raw.githubusercontent.com/hackr-sh/ghostty-shaders/cb6eb4b0d1a3101c869c62e458b25a826f9dcde3/cursor_blaze.glsl";
+          sha256 = "sha256-0g2lgqjdrn3c51glry7x2z30y7ml0y61arl5ykmf4yj0p85s5f41";
+        }}";
+        theme = "Banana Blueberry";
+        window-theme = "ghostty";
+        bell-features = "border";
+      };
     };
     password-store = {
       enable = true;
