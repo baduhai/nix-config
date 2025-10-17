7e02970b56c5da72ce06e9e44eba86054fc63b0f
William <baduhai@proton.me>
2025-10-17 15:39:56 -0300

    fix up secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,29 +1,29 @@
 let
-  io-user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO3Y0PVpGfJHonqDS7qoCFhqzUvqGq9I9sax+F9e/5cs";
-  io-host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKCIrKJk5zWzWEHvLMPMK8T3PyeBjsCsqzxPN+OrXfhA";
-  io = [
-    io-user
-    io-host
-  ];
+  io-user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO3Y0PVpGfJHonqDS7qoCFhqzUvqGq9I9sax+F9e/5cs user@io";
+  io = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKCIrKJk5zWzWEHvLMPMK8T3PyeBjsCsqzxPN+OrXfhA root@io";
 
-  rotterdam-user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA1v3+q3EaruiiStWjubEJWvtejam/r41uoOpCdwJtLL";
-  rotterdam-host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIjXcqQqlu03x2VVTdWOyxtKRszXAKX0AxTkGvF1oeJL";
-  rotterdam = [
-    rotterdam-user
-    rotterdam-host
-  ];
+  rotterdam-user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA1v3+q3EaruiiStWjubEJWvtejam/r41uoOpCdwJtLL user@rotterdam";
+  rotterdam = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIjXcqQqlu03x2VVTdWOyxtKRszXAKX0AxTkGvF1oeJL root@rotterdam";
 
-  alexandria-host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK95QueW+jp1ZmF299Xr3XkgHJ6dL7aZVsfWxqbOKVKA";
-  alexandria = [ alexandria-host ];
+  alexandria = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK95QueW+jp1ZmF299Xr3XkgHJ6dL7aZVsfWxqbOKVKA root@alexandria";
 
-  trantor-host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINkGuGLZPnYJbCGY4BhJ9uTupp6ruuR1NZ7FEYEaLPA7";
-  trantor = [ trantor-host ];
-
-  desktops = io ++ rotterdam;
-  servers = alexandria ++ trantor;
-  all-hosts = desktops ++ servers;
+  trantor = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINkGuGLZPnYJbCGY4BhJ9uTupp6ruuR1NZ7FEYEaLPA7 root@alexandria";
 in
+
 {
-  "cloudflare.age".publicKeys = all-hosts;
-  "webdav.age".publicKeys = all-hosts;
+  "cloudflare.age".publicKeys = [
+    io-user
+    rotterdam-user
+    alexandria
+  ];
+  "nextcloud-adminpass.age".publicKeys = [
+    io-user
+    rotterdam-user
+    alexandria
+  ];
+  "nextcloud-secrets.json.age".publicKeys = [
+    io-user
+    rotterdam-user
+    alexandria
+  ];
 }
