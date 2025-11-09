095d881ad995325d8a1ad1ecd40c510941562df0
William <baduhai@proton.me>
2025-11-09 00:00:00 -0300

    no ghostty notifications

--- users/modules/desktop/desktop.nix
+++ users/modules/desktop/desktop.nix
@@ -22,7 +22,7 @@
           url = "https://raw.githubusercontent.com/hackr-sh/ghostty-shaders/cb6eb4b0d1a3101c869c62e458b25a826f9dcde3/cursor_blaze.glsl";
           sha256 = "sha256:0g2lgqjdrn3c51glry7x2z30y7ml0y61arl5ykmf4yj0p85s5f41";
         }}";
-        bell-features = "border";
+        bell-features = "";
         gtk-titlebar-style = "tabs";
         keybind = [ "shift+enter=text:\\x1b\\r" ];
       };
