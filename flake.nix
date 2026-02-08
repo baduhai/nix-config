d482fd06945b6c3422b998552472cff21e6dde0e
William <baduhai@proton.me>
2026-02-08 14:02:12 -0300

    flake file rearranging

--- flake.nix
+++ flake.nix
@@ -2,58 +2,52 @@
   description = "My nix hosts";
 
   inputs = {
+    # nix tools
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
 
+    # nixos/hm
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
-
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # nixos/hm functionality modules
     agenix = {
       url = "github:ryantm/agenix";
       inputs.nixpkgs.follows = "nixpkgs-stable";
     };
-
     disko.url = "github:nix-community/disko";
-
-    noctalia = {
-      url = "github:noctalia-dev/noctalia-shell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
+    impermanence.url = "github:nix-community/impermanence";
+    nixos-cli.url = "github:nix-community/nixos-cli";
+    nix-flatpak.url = "github:gmodena/nix-flatpak/main";
     stylix.url = "github:danth/stylix";
 
-    nixos-cli.url = "github:nix-community/nixos-cli";
-
-    nix-flatpak.url = "github:gmodena/nix-flatpak/main";
-
-    zen-browser.url = "github:0xc000022070/zen-browser-flake";
-
-    impermanence.url = "github:nix-community/impermanence";
-
-    deploy-rs.url = "github:serokell/deploy-rs";
-
+    # nixos/hm program modules
     niri-flake.url = "github:sodiboo/niri-flake";
-
-    niri.url = "github:baduhai/niri/auto-center-when-space-available";
-
+    nix-ai-tools.url = "github:numtide/llm-agents.nix";
     nix-index-database = {
       url = "github:nix-community/nix-index-database";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    noctalia = {
+      url = "github:noctalia-dev/noctalia-shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    vicinae.url = "github:vicinaehq/vicinae";
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
 
+    # stand-alone tools
+    deploy-rs.url = "github:serokell/deploy-rs";
     terranix = {
       url = "github:terranix/terranix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nix-ai-tools.url = "github:numtide/llm-agents.nix";
-
-    vicinae.url = "github:vicinaehq/vicinae";
+    # others
+    niri.url = "github:baduhai/niri/auto-center-when-space-available";
   };
 
   outputs =
@@ -73,12 +67,13 @@
       imports = [
         flake-parts.flakeModules.modules
         inputs.terranix.flakeModule
-      ] ++ aspectsModule.imports
-        ++ packagesModule.imports
-        ++ shellsModule.imports
-        ++ terranixModule.imports
-        ++ [
-          ./deploy.nix
-        ];
+      ]
+      ++ aspectsModule.imports
+      ++ packagesModule.imports
+      ++ shellsModule.imports
+      ++ terranixModule.imports
+      ++ [
+        ./deploy.nix
+      ];
     };
 }
