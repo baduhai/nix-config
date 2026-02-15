755937cb56f26b90e4a551de9b20e26ec72f948d
William <baduhai@proton.me>
2026-02-15 16:45:33 -0300

    rework user aspect

--- aspects/users/user.nix
+++ aspects/users/user.nix
@@ -1,4 +1,4 @@
-{ inputs, self, ... }:
+{ inputs, lib, ... }:
 
 {
   flake = {
@@ -30,37 +30,6 @@
         };
         modules = [
           { nixpkgs.overlays = [ inputs.self.overlays.default ]; }
-
-          # CLI aspects (common module included)
-          inputs.self.modules.homeManager.cli-base
-          inputs.self.modules.homeManager.cli-btop
-          inputs.self.modules.homeManager.cli-comma
-          inputs.self.modules.homeManager.cli-direnv
-          inputs.self.modules.homeManager.cli-helix
-          inputs.self.modules.homeManager.cli-starship
-          inputs.self.modules.homeManager.cli-tmux
-
-          # Shell
-          inputs.self.modules.homeManager.shell-fish
-          inputs.self.modules.homeManager.shell-bash
-
-          # Desktop
-          inputs.self.modules.homeManager.desktop-desktop
-          inputs.self.modules.homeManager.desktop-niri
-
-          # Gaming
-          inputs.self.modules.homeManager.gaming-mangohud
-
-          # Programs
-          inputs.self.modules.homeManager.programs-media # for obs-studio
-
-          # Stylix
-          inputs.self.modules.homeManager.stylix
-
-          # User-specific (from _user/)
-          ./_user/git.nix
-
-          # Home configuration
           {
             home = {
               username = "user";
@@ -68,7 +37,17 @@
               stateVersion = "22.05";
             };
           }
-        ];
+          ((inputs.import-tree.initFilter (p: lib.hasSuffix ".nix" p)) ./_user)
+        ]
+        ++ (with inputs.self.modules.homeManager; [
+          base # base aspect
+          cli # cli aspect
+          desktop # desktop aspect
+
+          # other aspect
+          stylix
+          niri
+        ]);
       };
 
       "user@io" = inputs.home-manager.lib.homeManagerConfiguration {
@@ -79,31 +58,6 @@
         };
         modules = [
           { nixpkgs.overlays = [ inputs.self.overlays.default ]; }
-
-          # CLI aspects (common module included)
-          inputs.self.modules.homeManager.cli-base
-          inputs.self.modules.homeManager.cli-btop
-          inputs.self.modules.homeManager.cli-comma
-          inputs.self.modules.homeManager.cli-direnv
-          inputs.self.modules.homeManager.cli-helix
-          inputs.self.modules.homeManager.cli-starship
-          inputs.self.modules.homeManager.cli-tmux
-
-          # Shell
-          inputs.self.modules.homeManager.shell-fish
-          inputs.self.modules.homeManager.shell-bash
-
-          # Desktop
-          inputs.self.modules.homeManager.desktop-desktop
-          inputs.self.modules.homeManager.desktop-niri
-
-          # Stylix
-          inputs.self.modules.homeManager.stylix
-
-          # User-specific (from _user/)
-          ./_user/git.nix
-
-          # Home configuration
           {
             home = {
               username = "user";
@@ -111,7 +65,17 @@
               stateVersion = "22.05";
             };
           }
-        ];
+          ((inputs.import-tree.initFilter (p: lib.hasSuffix ".nix" p)) ./_user)
+        ]
+        ++ (with inputs.self.modules.homeManager; [
+          base # base aspect
+          cli # cli aspect
+          desktop # desktop aspect
+
+          # other aspect
+          stylix
+          niri
+        ]);
       };
     };
   };
