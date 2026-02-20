87d75380bb7a1661394f86cc58f00f89a45f47ad
William <baduhai@proton.me>
2026-02-20 11:14:31 -0300

    add beeper to web packages

--- aspects/desktop/web.nix
+++ aspects/desktop/web.nix
@@ -10,6 +10,7 @@
     {
       environment.systemPackages = with pkgs; [
         inputs.zen-browser.packages."${pkgs.stdenv.hostPlatform.system}".default
+        beeper
         bitwarden-desktop
         fragments
         nextcloud-client
