c3650b73a1310c61c2e67afe0d1347bc4dac852a
William <baduhai@proton.me>
2026-02-23 08:32:16 -0300

    opencode from nix-ai-tools

--- aspects/ai.nix
+++ aspects/ai.nix
@@ -4,8 +4,16 @@
     { inputs, pkgs, ... }:
     {
       environment.systemPackages =
-        (with pkgs; [ opencode ])
+        (with pkgs; [ ])
         ++ (with inputs.nix-ai-tools.packages.${pkgs.stdenv.hostPlatform.system}; [
+          opencode
         ]);
+
+      nix.settings = {
+        extra-substituters = [ "https://cache.numtide.com" ];
+        extra-trusted-public-keys = [
+          "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+        ];
+      };
     };
 }
