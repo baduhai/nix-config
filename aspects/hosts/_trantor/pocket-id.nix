0fa0c9869d66e566439206cf07935a056df202b2
William <baduhai@proton.me>
2026-04-22 18:58:44 -0300

    fix pocket-id secret

--- aspects/hosts/_trantor/pocket-id.nix
+++ aspects/hosts/_trantor/pocket-id.nix
@@ -1,17 +1,22 @@
 {
   config,
   lib,
+  inputs,
   ...
 }:
 
 {
   services.pocket-id = {
     enable = true;
-    environmentFile = "/etc/nixos/secrets/pocket-id.key";
+    environmentFile = config.age.secrets.pocket-id-key.path;
     settings = {
       APP_URL = "https://auth.baduhai.dev";
       TRUST_PROXY = true;
       ANALYTICS_DISABLED = true;
     };
+  };
+
+  age.secrets.pocket-id-key = {
+    file = "${inputs.self}/secrets/pocket-id.key.age";
   };
 }
