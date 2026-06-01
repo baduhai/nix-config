c1b1c098e3c6a29446abb2053dde1a7fc4342407
William <baduhai@proton.me>
2026-06-01 11:28:26 -0300

    rename credentialsFile to credentialFiles for nixpkgs 26.05

--- aspects/hosts/_alexandria/nginx.nix
+++ aspects/hosts/_alexandria/nginx.nix
@@ -24,7 +24,7 @@ in
       email = "baduhai@proton.me";
       dnsResolver = "1.1.1.1:53";
       dnsProvider = "cloudflare";
-      credentialsFile = config.age.secrets.cloudflare.path;
+      credentialFiles = config.age.secrets.cloudflare.path;
     };
     certs = acmeCerts;
   };
