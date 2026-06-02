82c1ee29c89893dd94e8a2a2d3dfc32cb88757be
William <baduhai@proton.me>
2026-06-02 08:14:22 -0300

    rss: use fusion module instead of raw systemd service

--- aspects/hosts/_alexandria/rss.nix
+++ aspects/hosts/_alexandria/rss.nix
@@ -2,57 +2,19 @@
 
 let
   mkNginxVHosts = inputs.self.lib.mkNginxVHosts;
-  fusionPkg = inputs.fusion.packages.${pkgs.system}.default;
 in
 
 {
   services = {
+    fusion = {
+      enable = true;
+      package = inputs.fusion.packages.${pkgs.system}.default;
+      port = 58000;
+      allowPrivateFeeds = true;
+    };
+
     nginx.virtualHosts = mkNginxVHosts {
       domains."rss.baduhai.dev".locations."/".proxyPass = "http://localhost:58000/";
     };
   };
-
-  systemd.services.fusion = {
-    description = "Fusion RSS Reader";
-    after = [ "network-online.target" ];
-    wants = [ "network-online.target" ];
-    wantedBy = [ "multi-user.target" ];
-
-    environment = {
-      FUSION_PORT = "58000";
-      FUSION_DB_PATH = "/var/lib/fusion/fusion.db";
-      FUSION_FEVER_USERNAME = "fusion";
-      FUSION_PULL_INTERVAL = "1800";
-      FUSION_PULL_TIMEOUT = "30";
-      FUSION_PULL_CONCURRENCY = "10";
-      FUSION_PULL_MAX_BACKOFF = "172800";
-      FUSION_ALLOW_PRIVATE_FEEDS = "true";
-      FUSION_ALLOW_EMPTY_PASSWORD = "true";
-      FUSION_LOG_LEVEL = "INFO";
-    };
-
-    serviceConfig = {
-      ExecStart = "${fusionPkg}/bin/fusion";
-      DynamicUser = true;
-      StateDirectory = "fusion";
-      RuntimeDirectory = "fusion";
-      Restart = "on-failure";
-      RestartSec = 10;
-      ProtectSystem = "strict";
-      ProtectHome = true;
-      PrivateTmp = true;
-      PrivateDevices = true;
-      ProtectKernelTunables = true;
-      ProtectKernelModules = true;
-      ProtectControlGroups = true;
-      NoNewPrivileges = true;
-      RestrictRealtime = true;
-      RestrictNamespaces = true;
-      LockPersonality = true;
-      MemoryDenyWriteExecute = true;
-      SystemCallArchitectures = "native";
-      SystemCallFilter = [ "@system-service" "~@privileged" ];
-    };
-  };
-
 }
